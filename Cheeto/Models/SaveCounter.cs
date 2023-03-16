f29e074964747838923d598682f81ae8c73431f3
Kevin Moens <kmac@kevins-air.lan>
2023-03-15 19:58:44 -0500

    initial commit

---
+++ Cheeto/Models/SaveCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Cheeto.Models
+{
+	public class SaveCounter
+	{
+		public SaveCounter()
+		{
+		}
+
+		public int CrunchyCounter { get; set; } = 0;
+        public int PuffyCounter { get; set; } = 0;
+        public int BakedCounter { get; set; } = 0;
+        public int FlaminHotCounter { get; set; } = 0;
+        public int PawsCounter { get; set; } = 0;
+        public int CheddarPuffCounter { get; set; } = 0;
+        public int CheddarCrunchCounter { get; set; } = 0;
+        public int HotAsteroidsCounter { get; set; } = 0;
+        public int OtherCounter { get; set; } = 0;
+    }
+}
+
---
+++ Cheeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Cheeto;
+using Cheeto.Models;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<SaveCounter>();
+
+await builder.Build().RunAsync();
+
