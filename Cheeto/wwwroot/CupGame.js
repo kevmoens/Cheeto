f7161c7845243da36ee8590446c8f68818445e21
Kevin Moens <kevmoens@yahoo.com>
2023-05-10 21:56:58 -0500

    complete cup game

---
+++ Cheeto/wwwroot/CupGame.js
@@ -0,0 +1,6 @@
+﻿
+function resetImage() {
+    document.querySelector('#img1').classList.remove('fade-move');
+    document.querySelector('#img2').classList.remove('fade-move');
+    document.querySelector('#img3').classList.remove('fade-move');
+}
