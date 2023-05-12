37990f64a7cdad8dfc71859a20ce6c85b39c3eda
Kevin Moens <kevmoens@yahoo.com>
2023-05-11 21:32:03 -0500

    avoid speed clicking issue on cup game

--- Cheeto/wwwroot/CupGame.js
+++
@@ -1,6 +0,0 @@
-﻿
-function resetImage() {
-    document.querySelector('#img1').classList.remove('fade-move');
-    document.querySelector('#img2').classList.remove('fade-move');
-    document.querySelector('#img3').classList.remove('fade-move');
-}
